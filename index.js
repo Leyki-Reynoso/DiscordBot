27ad294754942e5d20252c35d6316f1b49578b96
Leyki Reynoso jr <66690518+Leyki-Reynoso@users.noreply.github.com>
2025-07-30 19:50:09 +0000

    uploading stuff

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Project is running!');
+})
+
+app.get('/', (req, res) => {
+  res.send("Hello World!");
+})
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds,     GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.on("messageCreate", message => {
+  if(message.content === "ping") {
+    message.channel.send("pong");
+  }
+})
+
+client.login(process.env['token']);
