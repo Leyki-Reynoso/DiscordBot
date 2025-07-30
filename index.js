dc7d5107f0376652de8752d74631281911d61a6d
Leyki Reynoso jr <66690518+Leyki-Reynoso@users.noreply.github.com>
2025-07-30 15:52:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-const express = require('express');
-const app = express();
-
-app.listen(3000, () => {
-  console.log('Project is running!');
-})
-
-app.get('/', (req, res) => {
-  res.send("Hello World!");
-})
-
 const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
   intents: [GatewayIntentBits.Guilds,     GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on("messageCreate", message => {
